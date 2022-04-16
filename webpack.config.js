5a8a8b8f96fd5f246a719c4e8d6ad6aa1572dd25
perfect perfect <gilberto.j.covarrubias@gmail.com>
2022-04-15 23:54:03 -0400

    working through section 19.4

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,58 @@
 const path = require("path");
 const webpack = require("webpack");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin
 
 module.exports = {
     // the entry point is
     //  - the root of the bundle and the beggining
     //  - and the beginning of the dependency graph
     //  - so give it the relative path to the client's code
-    entry:'./assets/js/script.js',
+    entry: {
+        app: "./assets/js/script.js",
+        events: "./assets/js/events.js",
+        schedule: "./assets/js/schedule.js",
+        tickets: "./assets/js/tickets.js"
+    },
     //  - webpack will bundle that code and 'output' that bundled code to a folder that we specify
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
+        filename: "[name].bundle.js",
+        path: __dirname + "/dist",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jpg$/i,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            // we need to change the names of the images output
+                            esModule: false,
+                            name (file) {
+                                return "[path][name].[ext]"
+                            },
+                            // creates a folder 'assets/img' to place the images
+                            publicPath: function(url) {
+                                return url.replace("../", "/assets/")
+                            }
+                        }
+                    },
+                    // file-loader processes the images first so that image-webpack-loader can optimize the emitted files. so make sure 'loader' is listed in the previous object
+                    { 
+                        loader: 'image-webpack-loader'
+                    }
+                ]
+            }
+        ]
     },
     plugins: [
         new webpack.ProvidePlugin({
             $: "jquery",
             jQuery: "jquery"
         }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+        })
     ],
     mode: 'development'
 };
